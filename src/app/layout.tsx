e2595de2c870f7e2fc6d0f0bcfda9353132f2b88
Nam-Dev-193127 <nnam16583@gmail.com>
2024-01-06 13:53:39 +0700

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal/>
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+        </body>
+    </html>
+  )
+}
